48e4cf5f73756d50d5d64b0845d8e18b3146576d
MGECOMMERCE <130738214+MGECOMMERCE@users.noreply.github.com>
2023-04-14 09:37:52 +0200

    Top2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './style.css';
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz! Top</h1>
+      <h1>Hello StackBlitz! Top2</h1>
       <p>Start editing to se koonfhkln e some magfnhgf gic hapftollpen :)</p>
     </div>
   );
